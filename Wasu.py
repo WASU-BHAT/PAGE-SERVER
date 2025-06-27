32f95150fc309452493ed06d6a5f698d5c1c6bcb
WASU-BHAT <bhatwasy092@gmail.com>
2025-06-27 11:09:12 +0530

    Update Wasu.py

--- Wasu.py
+++ Wasu.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>🥀🥀𝐓𝐇𝐄 𝐋𝐄𝐆𝐄𝐍𝐃 𝐏𝐑𝐈𝐍𝐂𝐄 𝐇𝐄𝐑𝐄🥀🥀
+  <title>🥀🥀GAAND TOD SERVER🥀🥀
 </title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
